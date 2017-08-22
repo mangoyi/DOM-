bdd1de79914c0e3568592f9d4ab433e940ca9023
mangoyiy <mangoyiy>
2017-08-22 15:40:32 +0800

    src

--- showPic.js
+++ showPic.js
@@ -28,7 +28,7 @@ function prepareGallery() {
 function preparePlaceholder() {
     var placeholder = document.createElement('img');
     placeholder.setAttribute("id", "placeholder");
-    placeholder.setAttribute("src", "placeholder.jpg");
+    placeholder.setAttribute("src", "images/placeholder.jpg");
     placeholder.setAttribute("alt", "my image gallery");
     var description = document.createElement('p');
     description.setAttribute("id", "description");
