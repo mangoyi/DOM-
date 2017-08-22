ef0e6bace4b9e2198bddecf04e33a467354e08c7
mangoyi <759945821@qq.com>
2017-08-22 10:14:02 +0800

    createElement

--- example.js
+++ example.js
@@ -4,3 +4,17 @@ function insertParagraph(text) {
     str += '</p>';
     document.write(str);
 }
+
+window.onload = function() {
+    var para = document.createElement('p');
+    var txt1 = document.createTextNode('This is');
+    para.appendChild(txt1);
+    var em = document.createElement('em');
+    var txt2 = document.createTextNode(' my ')
+    em.appendChild(txt2);
+    para.appendChild(em);
+    var txt3 = document.createTextNode('content.');
+    para.appendChild(txt3);
+    var testdiv = document.getElementById('testdiv');
+    testdiv.appendChild(para);
+}
