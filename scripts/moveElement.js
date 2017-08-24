2512f06ce186872be27862e0816298dd1554fa6f
mangoyi <759945821@qq.com>
2017-08-24 16:10:18 +0800

    Optimize animate

--- scripts/moveElement.js
+++ scripts/moveElement.js
@@ -40,3 +40,51 @@ function moveElement(elementID, final_x, final_y, interval) {
     这样一来，即使因为用户快速移动鼠标而使得某个元素需要向不同的方向移动，实际执行的也只有一条setTimeout函数条用语句。
 
 */
+
+// 动画优化
+/*
+    目前的状态是一步只能前进1px，但是想要的效果是如果元素距离目的地距离远，每次前进一大步。如果元素距离目的地距离近，每次前进一小步。
+
+    1. 计算出元素距离目的地之间的距离，计算出来距离是多少
+
+    2. 将元素朝目的地移动十分之一的距离(当然也可以选择其他的数字比如除9、8、7)
+
+    3. 当dist的值小于1的时候。而不可能把一个元素移动不到一个像素的距离。
+
+    4. 当然可以使用Math的ceil方法来解决。
+
+*/
+
+function moveElement(elementID, final_x, final_y, interval) {
+    var elem = document.getElementById(elementID);
+    if (elem.moveElement) {
+        clearTimeout(elem.moveElement);
+    }
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    var dist = 0;
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+    if (xpos < final_x) {
+        dist = Math.ceil((final_x -xpos)/10) ;  
+        xpos = xpos + dist;
+    }
+    if (xpos > final_x) {
+        dist = Math.ceil((xpos -final_x)/10) ;  
+        xpos = xpos - dist;
+    }
+    if (ypos < final_y) {
+        dist = Math.ceil((final_y -ypos)/10) ;  
+        ypos = ypos + dist;
+    }
+    if (ypos > final_y) {
+        dist = Math.ceil((ypos - final_y)/10) ;  
+        ypos = ypos - dist;
+    }
+    elem.style.left = xpos + 'px';
+    elem.style.top = ypos + 'px';
+    var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    // movement = setTimeout(repeat, interval);
+    elem.moveElement = setTimeout(repeat, interval);
+}
